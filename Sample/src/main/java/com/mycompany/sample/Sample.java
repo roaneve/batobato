4f4f97bbd47f5ba15a98a58dc119260a2fe350de
roaneve <sampangroan3@gmail.com>
2026-08-26 16:21:32 +0800

    Add files via upload

---
+++ Sample/src/main/java/com/mycompany/sample/Sample.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sample;
+
+/**
+ *
+ * @author roan sampang
+ */
+public class Sample {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
